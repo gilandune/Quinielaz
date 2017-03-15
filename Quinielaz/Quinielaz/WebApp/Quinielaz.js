dbe216f1a74b44b36f38534eed1212d8933020a8
lazuk182 <lazgonmon@gmail.com>
2017-03-15 11:49:36 -0600

    Quinielaz
    Se levanta el proyecto

---
+++ Quinielaz/Quinielaz/WebApp/Quinielaz.js
@@ -0,0 +1,35 @@
+﻿angular.module('Quinielaz', ['ngMaterial', 'ngRoute', 'ngCookies'])
+.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('blue', {
+            'default': '500',
+            'hue-1': '100',
+            'hue-2': '600',
+            'hue-3': 'A100'
+        })
+        .accentPalette('red')
+        .backgroundPalette('grey', {
+            'default': '200'
+        });
+})
+.config(function ($mdDateLocaleProvider) {
+    $mdDateLocaleProvider.months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+                                    'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    $mdDateLocaleProvider.shortMonths = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun',
+                                    'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+    $mdDateLocaleProvider.days = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado'];
+    $mdDateLocaleProvider.shortDays = ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'];
+    $mdDateLocaleProvider.firstDayOfWeek = 1;
+    $mdDateLocaleProvider.weekNumberFormatter = function (weekNumber) {
+        return 'Semana ' + weekNumber;
+    };
+    $mdDateLocaleProvider.msgCalendar = 'Calendario';
+    $mdDateLocaleProvider.msgOpenCalendar = 'Abrir calendario';
+    $mdDateLocaleProvider.formatDate = function (date) {
+        return moment(date).format('DD/MM/YYYY');
+    };
+    $mdDateLocaleProvider.parseDate = function (dateString) {
+        var m = moment(dateString, 'DD/MM/YYYY', true);
+        return m.isValid() ? m.toDate() : new Date(NaN);
+    };
+})
