447617f40c3d887908d2ee18a5eb9d76644d0b50
lazuk182 <lazgonmon@gmail.com>
2017-03-16 10:22:34 -0600

    Archivos de configuracion

--- Quinielaz/QuinielazCore/Usuarios/UsuariosController.cs
+++ Quinielaz/QuinielazCore/Usuarios/UsuariosController.cs
@@ -25,28 +25,29 @@ namespace QuinielazCore.Usuarios
             bool Success = false;
             try
             {
-                QuinielazDB = new quinielazdbEntities();
-                var _usr = (from em in QuinielazDB.tbl_usuarios
-                            where em.bitActivo == true && em.vchUsuario == user && em.vchPassword == password
-                            select new
-                            {
-                                Id = em.intUsuarioID,
-                                Nombre = em.vchNombre ?? "",
-                                _email = em.vchEmail ?? "",
-                                bitActivo = em.bitActivo,
-                                usuario = em.vchUsuario,
-                                pass = em.vchPassword
-                            }).First();
-                if (Success = _usr.Id > 0 ? true : false)
+                using (QuinielazDB = new quinielazdbEntities())
                 {
-                    User.Id = _usr.Id;
-                    User.Nombre = _usr.Nombre;
-                    User.Correo = _usr._email;
-                    User.Activo = _usr.bitActivo;
-                    User.UserName = _usr.usuario;
-                    Token = Security.Encrypt(_usr.Id + "|" + user + "|" + password);
+                    var _usr = (from em in QuinielazDB.tbl_usuarios
+                                where em.bitActivo == true && em.vchUsuario == user && em.vchPassword == password
+                                select new
+                                {
+                                    Id = em.intUsuarioID,
+                                    Nombre = em.vchNombre ?? "",
+                                    _email = em.vchEmail ?? "",
+                                    bitActivo = em.bitActivo,
+                                    usuario = em.vchUsuario,
+                                    pass = em.vchPassword
+                                }).First();
+                    if (Success = _usr.Id > 0 ? true : false)
+                    {
+                        User.Id = _usr.Id;
+                        User.Nombre = _usr.Nombre;
+                        User.Correo = _usr._email;
+                        User.Activo = _usr.bitActivo;
+                        User.UserName = _usr.usuario;
+                        Token = Security.Encrypt(_usr.Id + "|" + user + "|" + password);
+                    }
                 }
-                QuinielazDB.Dispose();
             }
             catch (Exception exc)
             {
@@ -163,7 +164,7 @@ namespace QuinielazCore.Usuarios
             catch (Exception exc)
             {
                 OutMessage = "Error en el sistema: " + exc.Message;
-                Log.EscribeLog("Error en UsuariosController.ActivarrUsuario(" + Id + "): " + exc.Message);
+                Log.EscribeLog("Error en UsuariosController.ActivarUsuario(" + Id + "): " + exc.Message);
             }
             return Actualizado;
         }
